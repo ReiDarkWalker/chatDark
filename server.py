748872f260b87dd1f5c06ac8fa2f1fed945c218c
Gabriel <email.darkpessoal@gmail.com>
2023-03-24 01:21:54 -0300

    Server é um server
    2.0 - servidor agora exerce apenas o papel de um servidor, aceita 2 clients e envia a mensagem de um para o outro

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-import tkinter as tk
 import socket
-import time
 import threading
+import time
 
 #definindo host e porta
 HOST = 'localhost'
@@ -10,70 +9,36 @@ PORT = 80
 #criando o objeto do socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen()
+s.listen(2) #espera por até 2 conexões (dois clients)
 
-print('Servidor ligado [°]')
+print('[°] Servidor Ligado')
 
-#aceita novas conexões e captura o ip e a porta
-conn, adress = s.accept()
-host, port = adress
+print('Aguardando conexões...')
 
-#cria um aviso no servidor para cada conexão
+#cria o objeto socket do primeiro client, captura seu ip e cria um log no servidor de conexão
+client_conn1, client_address1 = s.accept()
+host, port = client_address1
 timenow = time.strftime('[%H:%M:%S]')
 print(f'{timenow} {host} se conectou pela porta: {port}')
 
-#função para receber mensagens e printa-las na GUI
-def receber_mensagens():
-    while True:
-        data = conn.recv(1024).decode('utf-8')
-        if not data:
-            break
-        mensagens.configure(state=tk.NORMAL)
-        mensagens.insert(tk.END, "Usuário: " + data + "\n")
-        mensagens.configure(state=tk.DISABLED)
-        mensagens.see(tk.END)
+#cria o objeto socket do segundo client, captura seu ip e cria um log no servidor de conexão
+client_conn2 ,client_address2 = s.accept()
+host, port = client_address2
+timenow = time.strftime('[%H:%M:%S]')
+print(f'{timenow} {host} se conectou pela porta: {port}')
 
-#função para enviar mensagens e printa-las na GUI
-def enviar_mensagem(event=None):
-    mensagem = caixa_de_texto.get().strip()
-    if not mensagem:
-        return
-    conn.sendall(mensagem.encode('utf-8'))
+while True:
+    
+    def envio1(): #recebe o que o primeiro client envia, checa se ele está enviando algo, se sim, o envia para o segundo client
+        data_c1 = client_conn1.recv(1024) 
+        if data_c1:
+            client_conn2.sendall(data_c1)
 
-    mensagens.configure(state=tk.NORMAL)
-    mensagens.insert(tk.END, "Você: " + mensagem + "\n")
-    mensagens.configure(state=tk.DISABLED)
-    caixa_de_texto.delete(0, tk.END)
-    mensagens.see(tk.END)
+    def envio2(): #recebe o que o segundo client envia, checa se ele está enviando algo, se sim, o envia para o primeiro client
+        data_c2 = client_conn2.recv(1024)
+        if data_c2:
+            client_conn1.sendall(data_c2)
 
-#cria o objeto da GUI
-janela = tk.Tk()
-janela.title("chatDark")
-
-frame_mensagens = tk.Frame(janela)
-frame_mensagens.pack(fill=tk.BOTH, expand=True)
-
-scrollbar = tk.Scrollbar(frame_mensagens)
-scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-mensagens = tk.Text(frame_mensagens, wrap=tk.WORD, yscrollcommand=scrollbar.set)
-mensagens.pack(fill=tk.BOTH, expand=True)
-scrollbar.config(command=mensagens.yview)
-mensagens.configure(state=tk.DISABLED)
-
-frame_envio = tk.Frame(janela)
-frame_envio.pack(fill=tk.X, side=tk.BOTTOM)
-
-caixa_de_texto = tk.Entry(frame_envio)
-caixa_de_texto.pack(side=tk.LEFT, padx=5, pady=5, expand=True, fill=tk.X)
-caixa_de_texto.bind('<Return>', enviar_mensagem)
-
-botao_enviar = tk.Button(frame_envio, text="Enviar", command=enviar_mensagem)
-botao_enviar.pack(side=tk.RIGHT, padx=5, pady=5)
-
-#coloca o recebimento de mensagens como outra thread
-thread = threading.Thread(target=receber_mensagens)
-thread.start()
-
-#mantém a GUI em funcionamento
-janela.mainloop()
+    #coloca os 2 envios em threads separadas para poderem enviar normalmente sem limitações
+    threading.Thread(target=envio1).start()
+    threading.Thread(target=envio2).start()
